338d03b40ae3a5f069ed4fdf8f945b2b748c239f
rajughosh <raju.ghosh@fieldnation.com>
2017-02-28 16:16:31 +0600

    added bucket name

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
     region = "us-west-2"
 }
-resource "aws_s3_bucket" "rajuabcd" {
-    bucket = "raju-test-abcd"
+resource "aws_s3_bucket" "rajutestbucket" {
+    bucket = "raju-tf"
 }
